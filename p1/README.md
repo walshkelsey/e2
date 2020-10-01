26844719779ac931c016eb68b51fc9d4b009072a
Kelsey Walsh <walsh.kelseya@gmail.com>
2020-09-30 20:41:21 -0400

    Added README.md and bank files

--- p1/README.md
+++ p1/README.md
@@ -1,3 +1,10 @@
-#p1 README.md file
+#Project 1
+
+By: Kelsey Walsh
+Production URL: http://p1.kwalsh12.me/
+
+##Outside Resources
+https://www.php.net/manual/en/language.types.array.php
+
 
 
---
+++ practice/bank-view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>PHPiggy Bank</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png' style='width:202px;'>
+
+    <p>
+        You have $<?php echo $total; ?> in your piggy bank.
+   
+    You have <?php echo $quarter; ?> quarters in your bank.
+    </p>
+</body>
+</html>
